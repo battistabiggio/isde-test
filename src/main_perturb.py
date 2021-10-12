a72ed608b363f1137c3691b8d86c63275e6ad69a
bat <battista.biggio@unica.it>
2021-10-12 12:59:35 +0200

    Uploaded code.

--- src/main_perturb.py
+++ src/main_perturb.py
@@ -20,7 +20,7 @@ def robustness_test(clf, data_pert, param_name, param_values):
         y_pred = clf.predict(xp)
         # compute classification accuracy using y_pred
         clf_acc = np.mean(y_ts == y_pred)
-        print("Test accuracy(K=", k, "): ", int(clf_acc * 10000) / 100, "%")
+        # print("Test accuracy(K=", k, "): ", int(clf_acc * 10000) / 100, "%")
         test_accuracies[i] = clf_acc
     return test_accuracies
 
@@ -41,30 +41,36 @@ print("Number of total samples: ", x.shape[0],
 
 x_tr, y_tr, x_ts, y_ts = split_data(x, y, n_tr=n_tr)
 
-clf = NMC()
-clf.fit(x_tr, y_tr)
-y_pred = clf.predict(x_ts)
-clf_acc = np.mean(y_ts == y_pred)
-print("Test accuracy: ", int(clf_acc * 10000) / 100, "%")
-
 param_values = np.array([0, 10, 20, 50, 100, 200, 300, 400, 500])
 
+clf_list = [NMC(), SVC(kernel='linear')]
+clf_names = ['NMC', 'SVM']
 
 plt.figure(figsize=(10,5))
-test_accuracies = robustness_test(
-    clf,  CDataPerturbUniform(), param_name='K', param_values=param_values)
 
-plt.subplot(1, 2, 1)
-plt.plot(param_values, test_accuracies)
-plt.xlabel('K')
-plt.ylabel('Test accuracy(K)')
+for i, clf in enumerate(clf_list):
 
-test_accuracies = robustness_test(
-    clf,  CDataPerturbGaussian(), param_name='sigma', param_values=param_values)
+    clf.fit(x_tr, y_tr)
+    y_pred = clf.predict(x_ts)
+    clf_acc = np.mean(y_ts == y_pred)
+    print("Test accuracy: ", int(clf_acc * 10000) / 100, "%")
 
-plt.subplot(1, 2, 2)
-plt.plot(param_values, test_accuracies)
-plt.xlabel('sigma')
-plt.ylabel('Test accuracy(sigma)')
+    test_accuracies = robustness_test(
+        clf,  CDataPerturbUniform(), param_name='K', param_values=param_values)
+
+    plt.subplot(1, 2, 1)
+    plt.plot(param_values, test_accuracies, label=clf_names[i])
+    plt.xlabel('K')
+    plt.ylabel('Test accuracy(K)')
+    plt.legend()
+
+    test_accuracies = robustness_test(
+        clf,  CDataPerturbGaussian(), param_name='sigma', param_values=param_values)
+
+    plt.subplot(1, 2, 2)
+    plt.plot(param_values, test_accuracies, label=clf_names[i])
+    plt.xlabel(r'$\sigma$')
+    plt.ylabel(r'Test accuracy($\sigma$)')
+    plt.legend()
 
 plt.show()
